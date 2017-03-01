9dc08a3859a32a91a73797578a13b87f84b052ba
James <James@T100Laptop>
2017-03-01 09:40:36 -0400

    finished stage 1

--- emailvalidator/src/main/java/com/example/emailvalidator/App.java
+++ emailvalidator/src/main/java/com/example/emailvalidator/App.java
@@ -1,13 +1,42 @@
 package com.example.emailvalidator;
 
+import java.util.Scanner;
+import java.net.InetAddress;
+import java.net.UnknownHostException;  
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static Scanner kbd;
+	public static void main( String[] args ) {
+    	kbd = new Scanner(System.in);
+        System.out.println( "Enter an email address:" );
+        String input = kbd.nextLine();
+        //System.out.println("You entered:"+input);
+        if (validator(input))
+        	System.out.println("Success");
+        else
+        	System.out.println("Failed");
+        
+        
     }
+    public static Boolean validator (String email){
+    	Boolean check = true;
+    	if (!email.contains("@")){
+    		check = false;
+        	System.out.println("Email does not contain '@'");
+    	}
+    	if (!email.contains(".")){
+    		check = false;
+        	System.out.println("Email does not contain '.'");
+    	}
+    	
+    		
+    	return check;
+    }
+    
+    
 }
